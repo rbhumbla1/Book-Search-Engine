6804763a4a9ad66d9c28eb89daf9853f50cd92fc
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2023-03-29 21:14:11 -0700

    added mutations and queries on client side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,7 +11,7 @@ const typeDefs = gql`
 
   type Book {
     bookId: String
-    authors: String
+    authors: [String]
     description: String
     image: String
     link: String
@@ -20,7 +20,7 @@ const typeDefs = gql`
 
   input BookInput {
     bookId: String
-    authors: String
+    authors: [String]
     description: String
     image: String
     link: String
