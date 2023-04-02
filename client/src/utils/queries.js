09aa102d407c705c683e50b69a550e4d078014bf
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2023-04-02 12:13:40 -0700

    workingon saving book

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
 export const GET_ME = gql`
   query me {
     me {
